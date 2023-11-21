c389fdd1b260eb7d9c58b7382ba95b85aedb4b12
JoseDev <jotacastellanosguz@gmail.com>
2023-11-21 16:38:21 -0600

    se agrega constantes

---
+++ common-util/src/main/java/com/unistmo/ws/ConstantsWs.java
@@ -0,0 +1,12 @@
+package com.unistmo.ws;
+
+public class ConstantsWs {
+    public final static String BASE_EMPLOYEE_PATH_WS = "/employees";
+    public final static String EMPLOYEE_GET_ALL = "/get-all-employees";
+    public final static String EMPLOYEE_SAVE = "/new-employee";
+    public final static String BASE_PRODUCT_PATH_WS = "/products";
+    public static final String PRODUCT_SAVE = "/save-product";
+    public static final String PRODUCT_DELETE = "/delete-product/{productId}";
+    public static final String PRODUCT_UPDATE = "/update-product/{productId}";
+    public static final String PRODUCT_GET_ALL = "/get-all-products";
+}
