d6504395c612dc46ea41f682f40b6b277cbb58f1
JoseDev <jotacastellanosguz@gmail.com>
2024-01-03 11:37:33 -0600

    packaging changed and some topics added

--- web-service/src/main/java/com/unistmo/controller/employee/EmployeeController.java
+++ web-service/src/main/java/com/unistmo/controller/employee/EmployeeController.java
@@ -1,6 +1,19 @@
+/**
+ * This module is about impact of the final keyword on performance
+ * <p>
+ * This module explores  if there are any performance benefits from
+ * using the final keyword in our code. This module examines the performance
+ * implications of using final on a variable, method, and class level.
+ * </p>
+ *
+ * @since 1.0
+ * @author baeldung
+ * @version 1.1
+ */
 package com.unistmo.controller.employee;
 
 import com.unistmo.dto.EmployeeDTO;
+import com.unistmo.entity.Employee;
 import com.unistmo.service.employee.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,26 +29,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(BASE_EMPLOYEE_PATH_WS)
 public class EmployeeController {
 
-    private EmployeeService employeeService;
+    /**
+     *
+     */
+    private final EmployeeService employeeService;
 
+    /**
+     * Constructor.
+     * @param employeeService
+     */
     @Autowired
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController(final EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
+    /**
+     * Método GET para obtener todos los empleados.
+     * @return {@link ResponseEntity}
+     */
     @GetMapping(EMPLOYEE_GET_ALL)
-    public ResponseEntity<?> getAllEmployees(){
+    public ResponseEntity<List<Employee>> getAllEmployees() {
         return ResponseEntity.ok().body(employeeService.getEmployeesList());
     }
 
+    /**
+     * Métoo POST para guardar un nuevo empleado.
+     * @param employeeDTO
+     * @return {@link ResponseEntity}
+     */
     @PostMapping(EMPLOYEE_SAVE)
-    public ResponseEntity<?> saveNewEmployee(@RequestBody @Valid EmployeeDTO employeeDTO){
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.saveEmployee(employeeDTO));
+    public ResponseEntity<String> saveNewEmployee(@RequestBody @Valid final EmployeeDTO employeeDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(employeeService.saveEmployee(employeeDTO));
     }
 }
